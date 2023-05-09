747942f6d7c6e70ccb123889193846b75bd1f209
Duy Minh Lê <duyminhle@192.168.1.7>
2023-05-09 22:32:28 +0700

    feat: check riot account

--- index.js
+++ index.js
@@ -1,56 +1,96 @@
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 const creds = require("./google_cred.json");
-// const axios = require("axios");
+const axios = require("axios");
 require("dotenv").config();
 const puppeteer = require("puppeteer");
 
 const main = async () => {
- try {
-  console.log("START: READING FILE");
-  const sheetId = process.env.SHEET_ID;
-  console.log("🚀 ~ file: index.js:11 ~ main ~ sheetId:", sheetId)
-  const doc = new GoogleSpreadsheet(sheetId);
+  try {
+    console.log("START: READING FILE");
+    const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
+    await doc.useServiceAccountAuth(creds);
+    await doc.loadInfo();
+    const defaultSheet =
+      doc.sheetsByIndex[parseInt(process.env.SHEET_INDEX, 10)];
+    await defaultSheet.loadCells();
+    const lines = await defaultSheet.getRows();
+    let count = 0;
+    for (let line of lines.slice(1)) {
+      const username = defaultSheet.getCell(line.rowIndex - 2, 0).value;
+      const password = defaultSheet.getCell(line.rowIndex - 2, 1).value;
+      const code2F = defaultSheet.getCell(line.rowIndex - 2, 2).value;
+      const value = await axios.get(`https://2fa.live/tok/${code2F}`);
+      const resultCell = defaultSheet.getCell(line.rowIndex - 2, 3);
 
-  await doc.useServiceAccountAuth(creds);
-  await doc.loadInfo();
-  const defaultSheet = doc.sheetsByIndex[parseInt(process.env.SHEET_INDEX, 10)];
-  await defaultSheet.loadCells();
-  const lines = await defaultSheet.getRows();
+      console.log(username, password);
+      console.log("----------");
 
-  for (let line of lines) {
-   const username = defaultSheet.getCell(line.rowIndex - 2 , 0).value;
-   const password = defaultSheet.getCell(line.rowIndex - 2 , 1).value;
-   const resultCell = defaultSheet.getCell(line.rowIndex - 2 , 2);
-   console.log(username, password);
-   console.log('----------');
+      const browser = await puppeteer.launch({
+        executablePath: process.env.CHROME_PATH,
+        headless: false,
+      });
 
-  const browser = await puppeteer.launch({executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome', headless: false});
+      const page = await browser.newPage();
 
-  const page = await browser.newPage();
+      const client = await page.target().createCDPSession();
+      await client.send("Network.clearBrowserCookies");
+      await client.send("Network.clearBrowserCache");
+      await page.goto(process.env.PRIME_GAMING_LINK, {
+        waitUntil: "networkidle2",
+      });
 
-  await page.goto('https://vuigher.com/');
- 
-  await page.click('div.navbar-avatar')
-  await page.type('input[name="username"]', username);
-  await page.type('input[name="password"]', password);
-  // Set screen size
+      // click sign in button
+      await page.waitForSelector(
+        'button[aria-label="User dropdown and more options"]'
+      ); // wait for the button to appear
+      await page.click('button[aria-label="User dropdown and more options"]');
+      await page.click('button[data-a-target="user-dropdown__sign-in"]');
 
-  await page.click('input[name="submit');
+      console.log("Start redirect ...");
 
-  await page.waitForNavigation();
-  await page.click('div.navbar-avatar')
-  const greeting = await page.$eval('div.navbar-user-welcome span', element => element.textContent.trim());
-  if (greeting) {
-    console.log(greeting);
-    resultCell.value = 'oke';
-    await defaultSheet.saveUpdatedCells();
-  } else {
-    console.log('The page does not contain a div element with class "navbar-user-welcome"');
+      // Waiting load aws login
+      await page.waitForSelector('input[id="ap_email"]');
+
+      console.log("Signing in...");
+      await page.type('input[id="ap_email"]', username);
+      await page.type('input[id="ap_password"]', password);
+      await page.click('input[id="signInSubmit');
+
+      await page.waitForNavigation({ waitUntil: "networkidle2" });
+
+      if (value.data) {
+        try {
+          await page.type('input[id="auth-mfa-otpcode"]', value.data.token);
+          await page.click('input[id="auth-signin-button"]');
+          await page.waitForNavigation({ waitUntil: "networkidle2" });
+          let valueAccount;
+          try {
+            const displayNameElement = await page.$(
+              'p.tw-amazon-ember-regular[data-a-target="Customer3PDisplayName"]'
+            );
+            const displayNameValue = await displayNameElement.evaluate(
+              (element) => element.getAttribute("title")
+            );
+            valueAccount = `Account Link ${displayNameValue}`;
+          } catch (e) {
+            valueAccount = "New";
+          }
+
+          resultCell.value = valueAccount;
+          await defaultSheet.saveUpdatedCells();
+        } catch (e) {
+          // const error = await page.$('#auth-error-message-box');
+          resultCell.value = "error";
+          await defaultSheet.saveUpdatedCells();
+        }
+        count++;
+        await browser.close();
+      }
+    }
+    console.log("🚀 Count user processed:", count);
+  } catch (e) {
+    console.log(e);
   }
-  }
- } catch (e) {
-  console.log(e);
- }
-}
+};
 
 main();
